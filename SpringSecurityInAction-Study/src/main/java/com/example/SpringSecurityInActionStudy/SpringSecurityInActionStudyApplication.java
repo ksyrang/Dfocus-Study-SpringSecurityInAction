a423751034b2e2380571da93640e061b82048183
JS_Kim <95598552+ksyrang@users.noreply.github.com>
2023-12-20 17:50:30 +0900

    init
    Init Project

---
+++ SpringSecurityInAction-Study/src/main/java/com/example/SpringSecurityInActionStudy/SpringSecurityInActionStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecurityInActionStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityInActionStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityInActionStudyApplication.class, args);
+	}
+
+}
---
+++ SpringSecurityInAction-Study/src/test/java/com/example/SpringSecurityInActionStudy/SpringSecurityInActionStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecurityInActionStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityInActionStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
